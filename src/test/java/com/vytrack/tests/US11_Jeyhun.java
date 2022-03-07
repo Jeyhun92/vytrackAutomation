c5f2aa4e8140acea32aa41c594e7ffc8a92cc03c
Jeyhun92 <jeyhun.ismayilov92@gmail.com>
2022-03-06 20:26:10 -0500

    Updated version of vytrack project_03/06/2022

--- src/test/java/com/vytrack/tests/US11_Jeyhun.java
+++ src/test/java/com/vytrack/tests/US11_Jeyhun.java
@@ -2,26 +2,29 @@ package com.vytrack.tests;
 
 import com.vytrack.tests.base.TestBase;
 import com.vytrack.utilities.Driver;
+import com.vytrack.utilities.ExtraUtils.Sleep;
 import com.vytrack.utilities.VytrackUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class US11_Jeyhun extends TestBase {
 
 
 
-    @Test
     public void verify_manager_see_expected_text()  {
         VytrackUtils.loginAsStoreManger();
 
      WebElement fleetBtn = Driver.getDriver().findElement(By.xpath("(//span[@class='title title-level-1'])[2]"));
      fleetBtn.click();
 
-
+        Sleep.Zzz(3);
         WebElement odometerBtn = Driver.getDriver().findElement(By.linkText("Vehicle Odometer"));
         odometerBtn.click();
+
+        Sleep.Zzz(3);
         WebElement message=Driver.getDriver().findElement(By.xpath("(//div[@class='message'])[2]"));
         String expected_Result= "You do not have permission to perform this action.";
 
@@ -34,13 +37,15 @@ public class US11_Jeyhun extends TestBase {
     public void verify_defaultPageNumber_is1()  {
 
         VytrackUtils.loginAsDriver();
-
+        Sleep.Zzz(3);
         WebElement fleetBtn = Driver.getDriver().findElement(By.xpath("(//span[@class='title title-level-1'])[1]"));
         fleetBtn.click();
 
+        Sleep.Zzz(3);
         WebElement odometerBtn = Driver.getDriver().findElement(By.linkText("Vehicle Odometer"));
         odometerBtn.click();
 
+        Sleep.Zzz(3);
         WebElement pageNumber = Driver.getDriver().findElement(By.xpath("//input[@type='number']"));
         System.out.println("pageNumber.attribute() = " + pageNumber.getAttribute("value"));
 
@@ -51,14 +56,15 @@ public class US11_Jeyhun extends TestBase {
     public void verify_default_view_perPage_is25() {
 
         VytrackUtils.loginAsDriver();
-
+        Sleep.Zzz(3);
         WebElement fleetBtn = Driver.getDriver().findElement(By.xpath("(//span[@class='title title-level-1'])[1]"));
         fleetBtn.click();
 
-
+        Sleep.Zzz(3);
         WebElement odometerBtn = Driver.getDriver().findElement(By.linkText("Vehicle Odometer"));
         odometerBtn.click();
 
+        Sleep.Zzz(3);
         WebElement drop = Driver.getDriver().findElement(By.xpath("//button[@class='btn dropdown-toggle ']"));
 
         System.out.println("drop = " + drop.getText());
