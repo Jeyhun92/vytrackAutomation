2dadf5a8c09d9ae7032ebc0c58a7e5ac82c0b69f
oz_ah <oz_ahmed@hotmail.com>
2022-03-07 22:13:51 -0500

    All data provider util class

---
+++ src/test/java/com/vytrack/utilities/ExtraUtils/All_DP.java
@@ -0,0 +1,44 @@
+package com.vytrack.utilities.ExtraUtils;
+
+import org.testng.annotations.DataProvider;
+
+public class All_DP {
+    @DataProvider(name = "DriverLogin")
+    Object[][] TruckDriver() {
+
+        return new Object[][]{
+                {"user44"},
+                {"user49"}
+        };
+
+    }
+    @DataProvider(name = "SalesManagerLogin")
+    Object[][] SalesManager() {
+
+        return new Object[][]{
+                {"storemanager75"},
+                {"storemanager78"}
+        };
+
+    }
+    @DataProvider(name = "StoreManagerLogin")
+    Object[][] StoreManager() {
+        return new Object[][]{
+                {"salesmanager133"}, {"salesmanager136"}
+
+        };
+    }
+    @DataProvider(name = "LoginAll")
+    Object[][] loginCredentials() {
+
+        return new Object[][]{
+                {"user44"},
+                {"user49"},
+                {"salesmanager133"},
+                {"salesmanager136"},
+                {"storemanager75"},
+                {"storemanager78"}
+        };
+
+    }
+}
