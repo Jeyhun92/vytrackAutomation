a94d4073bbfe00e2f67896768d77768fed863836
oz_ah <oz_ahmed@hotmail.com>
2022-03-11 13:13:58 -0500

    update

--- src/test/java/com/vytrack/utilities/ExtraUtils/HeaderMap.java
+++ src/test/java/com/vytrack/utilities/ExtraUtils/HeaderMap.java
@@ -21,21 +21,10 @@ public class HeaderMap {
         getDriver().findElement(By.linkText(subName)).click();
     }
 
-    public static void goToPage(String dropDownName){
-        ArrayList<String> headerLists = new ArrayList<>();
-
-        Driver.getDriver().findElements(By.xpath("//div[@id='main-menu']/ul/li")).forEach(p->headerLists.add(p.getText()));
-
-        if(headerLists.size()>4){
-        getDriver().findElement(By.xpath("(//div[@id='main-menu']/ul/li)[5]")).click();
-        getDriver().findElement(By.linkText(dropDownName)).click();}
-        else{getDriver().findElement(By.xpath("(//div[@id='main-menu']/ul/li)[3]")).click();
-            getDriver().findElement(By.linkText(dropDownName)).click();
-
-        }
 
 
-    }
+
+
 
 
 }
