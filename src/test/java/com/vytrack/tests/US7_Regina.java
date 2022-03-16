bd416f8d52dd640cce09a024ac6eb4bb25df2fbd
Regina <musovirova@yahoo.com>
2022-03-15 22:53:47 -0400

    Change data provider for US7

--- src/test/java/com/vytrack/tests/US7_Regina.java
+++ src/test/java/com/vytrack/tests/US7_Regina.java
@@ -21,46 +21,11 @@ public class US7_Regina extends TestBase{
     //AC #2: user check the first checkbox to check all the cars
     //AC #3: users can select any car
 
-    @Test(dataProvider = "DriverLogin", dataProviderClass = All_DP.class)
-    public void TC1_verify_that_driver_select_any_car(String truckDriverUserName) {
+    @Test(dataProvider = "LoginAll", dataProviderClass = All_DP.class)
+    public void TC1_verify_that_driver_select_any_car(String user) {
 
         //Verify that User launched to the Vehicle page, all the checkboxes should be unchecked
-        loginBy(truckDriverUserName);
-        Sleep.Zzz(3);
-
-
-
-        //Driver.getDriver().findElement(By.xpath("//div[@id='main-menu']//span[contains(text(),'Fleet')][1]")).click();
-        Sleep.Zzz(3);
-        Driver.getDriver().findElement(By.xpath("//li[@class='dropdown-menu-single-item first']//span[contains(text(),'Vehicles')]")).click();
-        WebElement checkbox = Driver.getDriver().findElement(By.xpath("//button[@class='btn btn-default btn-small dropdown-toggle']/input[@type='checkbox']"));
-        Assert.assertFalse(checkbox.isSelected(),"Checkbox is selected");
-
-        //AC2_Verify that User check the first checkbox to check all the cars
-        checkbox.click();
-        Assert.assertTrue(checkbox.isSelected(),"Checkbox is not selected");
-        //checkbox.click();
-
-        //AC3Verify that Users can select any car
-        List<WebElement> randomCheckbox = Driver.getDriver().findElements(By.xpath("//input[@tabindex='-1']"));
-        //Select $ UnSelect EachCheckBox
-        for (WebElement eachCheckbox : randomCheckbox) {
-            if (!eachCheckbox.isSelected()){
-                eachCheckbox.click();
-            }
-            for (WebElement eachUncheckBox : randomCheckbox) {
-                if (eachUncheckBox.isSelected()){
-                    eachUncheckBox.click();
-                }
-            }
-        }
-
-    }
-    @Test(dataProvider = "SalesManagerLogin", dataProviderClass = All_DP.class)
-    public void TC1_verify_that_salesmanager_select_any_car(String salesManagerUserName) {
-
-        //Verify that User launched to the Vehicle page, all the checkboxes should be unchecked
-        loginBy(salesManagerUserName);
+        loginBy(user);
         Sleep.Zzz(3);
         dashboard.moduleFleet.click();
         Sleep.Zzz(3);
@@ -88,44 +53,5 @@ public class US7_Regina extends TestBase{
         }
 
     }
-
-    @Test(dataProvider = "StoreManagerLogin", dataProviderClass = All_DP.class)
-    public void TC1_verify_that_storemanager_select_any_car(String storeManagerLogin){
-
-        //Verify that User launched to the Vehicle page, all the checkboxes should be unchecked
-        loginBy(storeManagerLogin);
-        Sleep.Zzz(3);
-        Driver.getDriver().findElement(By.xpath("//div[@id='main-menu']//span[contains(text(),'Fleet')][1]")).click();
-        Sleep.Zzz(3);
-        Driver.getDriver().findElement(By.xpath("//li[@class='dropdown-menu-single-item first']//span[contains(text(),'Vehicles')]")).click();
-        WebElement checkbox = Driver.getDriver().findElement(By.xpath("//button[@class='btn btn-default btn-small dropdown-toggle']/input[@type='checkbox']"));
-        Assert.assertFalse(checkbox.isSelected(),"Checkbox is selected");
-
-        //AC2_Verify that User check the first checkbox to check all the cars
-        checkbox.click();
-        Assert.assertTrue(checkbox.isSelected(),"Checkbox is not selected");
-        checkbox.click();
-
-        //AC3Verify that Users can select any car
-        List<WebElement> randomCheckbox = Driver.getDriver().findElements(By.xpath("//input[@tabindex='-1']"));
-        //Select $ UnSelect EachCheckBox
-        for (WebElement eachCheckbox : randomCheckbox) {
-            if (!eachCheckbox.isSelected()) {
-                eachCheckbox.click();
-                System.out.println(eachCheckbox + "checked");
-
-            }
-            for (WebElement eachUncheckBox : randomCheckbox) {
-                if (eachUncheckBox.isSelected()) {
-                    eachUncheckBox.click();
-                    System.out.println(eachCheckbox + "unchecked");
-                }
-            }
-
-        }
-
-
-    }
-
 
 }
